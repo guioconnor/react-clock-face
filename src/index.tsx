3093f3485273cee02c4ef1132492b0ce8fdcccbd
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2020-07-17 23:40:12 +0100

    Working clock

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,214 @@
-import * as React from 'react'
-import './styles.scss'
+import * as React from 'react';
+import styled from 'styled-components';
+
+interface StyleProps {
+  hours: number;
+  minutes: number;
+  size: string;
+  marks: boolean;
+  numbers: boolean;
+};
+
+const SVG = styled.svg`
+  height: ${(props: StyleProps) => props.size};
+  fill: none;
+  stroke: #000;
+  stroke-width: 1;
+  stroke-linecap: round;
+  transform: rotate(-90deg);
+  --start-minutes: 0;
+  --start-hours: 11;
+  --display--marks: ${(props: StyleProps) => props.marks ? "block" : "none"};
+  --display--numbers: ${(props: StyleProps) => props.numbers ? "block" : "none"};
+
+  circle {
+    fill: white;
+  }
+  
+  .marks {
+    transform: translate(20px, 20px);
+    stroke-width: 0.2;
+  }
+  
+  .minute,
+  .hour
+  {
+    transform: translate(20px, 20px) rotate(0deg);
+    transition: transform 1s;
+  }
+  
+  .minute {
+    transform: translate(20px, 20px) rotate(${(props: StyleProps) => 360 * props.hours + props.minutes * 6}deg);
+    stroke-width: 0.6;
+  }
+  
+  .hour {
+    transform: translate(20px, 20px) rotate(${(props: StyleProps) => props.hours * 30 + props.minutes * .5}deg);
+    stroke-width: 1;
+  }
+  
+  .tiaText {
+    font-size: 1px;
+    font-family: sans-serif;
+    transform: translate(14px, 19px) rotate(90deg);
+    fill: #dfdfdf;
+    stroke: none;
+  }
+  
+  /* numbers */
+  .numbers {
+    display: var(--display--numbers);
+    transform: translate(20px, 20px) rotate(90deg);
+  }
+  
+  .numbers text {
+    text-anchor: middle;
+    font-size: 3px;
+    font-family: sans-serif;
+    fill: black;
+    stroke: none;
+    transform: translate(0, -15px);
+    
+  }
+  
+  .numbers > g:nth-child(1) {
+    transform: rotate(30deg);
+  }
+  
+  .numbers > g:nth-child(2) {
+    transform: rotate(calc(2 * 30deg));
+  }
+  
+  .numbers > g:nth-child(3) {
+    transform: rotate(calc(3 * 30deg));
+  }
+  
+  .numbers > g:nth-child(4) {
+    transform: rotate(calc(4 * 30deg));
+  }
+  .numbers > g:nth-child(5) {
+    transform: rotate(calc(5 * 30deg));
+  }
+  
+  .numbers > g:nth-child(6) {
+    transform: rotate(calc(6 * 30deg));
+  }
+  
+  .numbers > g:nth-child(7) {
+    transform: rotate(calc(7 * 30deg));
+  }
+  
+  .numbers > g:nth-child(8) {
+    transform: rotate(calc(8 * 30deg));
+  }
+  
+  .numbers > g:nth-child(9) {
+    transform: rotate(calc(9 * 30deg));
+  }
+  
+  .numbers > g:nth-child(10) {
+    transform: rotate(calc(10 * 30deg));
+  }
+  
+  .numbers > g:nth-child(11) {
+    transform: rotate(calc(11 * 30deg));
+  }
+  .numbers > g:nth-child(12) {
+    transform: rotate(calc(12 * 30deg));
+  }
+  
+  
+  
+  .pin {
+    stroke: black;
+    stroke-width: 0.2;
+  }
+  
+  
+  /* marks */
+  .marks {
+    display: var(--display--marks);
+  }
+  
+  .marks > line:nth-child(1) {
+    transform: rotate(30deg);
+  }
+  
+  .marks > line:nth-child(2) {
+    transform: rotate(calc(2 * 30deg));
+  }
+  
+  .marks > line:nth-child(3) {
+    transform: rotate(calc(3 * 30deg));
+    stroke-width: 0.5;
+  }
+  
+  .marks > line:nth-child(4) {
+    transform: rotate(calc(4 * 30deg));
+  }
+  .marks > line:nth-child(5) {
+    transform: rotate(calc(5 * 30deg));
+  }
+  
+  .marks > line:nth-child(6) {
+    transform: rotate(calc(6 * 30deg));
+    stroke-width: 0.5;
+  }
+  
+  .marks > line:nth-child(7) {
+    transform: rotate(calc(7 * 30deg));
+  }
+  
+  .marks > line:nth-child(8) {
+    transform: rotate(calc(8 * 30deg));
+  }
+  
+  .marks > line:nth-child(9) {
+    transform: rotate(calc(9 * 30deg));
+    stroke-width: 0.5;
+  }
+  
+  .marks > line:nth-child(10) {
+    transform: rotate(calc(10 * 30deg));
+  }
+  
+  .marks > line:nth-child(11) {
+    transform: rotate(calc(11 * 30deg));
+  }
+  .marks > line:nth-child(12) {
+    transform: rotate(calc(12 * 30deg));
+    stroke-width: 0.5;
+  }
+
+  `;
 
 export type ICounterProps = {
-  className?: string
+  className?: string,
+  hours?: number,
+  minutes?: number,
+  marks?: boolean,
+  numbers?: boolean,
+  size?: string,
 }
 
-const App: React.FC<ICounterProps> = ({ className = '' }) => {
-
+const App: React.FC<ICounterProps> = ({
+  className = '',
+  hours = 0,
+  minutes =  0,
+  marks = false,
+  numbers = false,
+  size = "100vh",
+}) => {
+  console.log(hours, minutes);
   return (
-    <svg
+    <SVG
       viewBox="0 0 40 40"
       className={className}
+      hours={hours}
+      minutes={minutes}
+      marks={marks}
+      numbers={numbers}
+      size={size}
     >
       <circle cx="20" cy="20" r="19" />
       <g className="marks">
@@ -70,7 +268,7 @@ const App: React.FC<ICounterProps> = ({ className = '' }) => {
       <line x1="0" y1="0" x2="9" y2="0" className="hour" />
       <line x1="0" y1="0" x2="13" y2="0" className="minute" />
       <circle cx="20" cy="20" r="0.7" className="pin" />
-    </svg>
+    </SVG>
   )
 }
 
